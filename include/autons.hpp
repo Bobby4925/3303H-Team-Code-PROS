2600f7e82f5a727914d55aed1d6abb0c0336e327
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2024-09-25 18:56:23 -0700

    Fixed Code

---
+++ include/autons.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+void togglePiston();
+
+void moveIntake(bool val);
+
+void moveBelt(bool var);
+
+void grab();
+
+void stop();
+
+// void baseAuton();
+
+void autonOne();
+void angularTesting(); 
---
+++ src/autons.cpp
@@ -0,0 +1,55 @@
+#include "autons.hpp"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "config.hpp"
+
+// ASSET(basicAuton_txt);
+
+void togglePiston(){
+	Piston.toggle(); 
+}
+void moveIntake(bool val){
+	if(val == true){
+		Intake.move_velocity(200);
+	}
+	else{
+		Intake.move_velocity(-200);
+	}
+}
+void moveBelt(bool val){
+	if(val == true){
+		Belt.move_velocity(600);
+	}
+	else{
+		Belt.move_velocity(-600);
+	}
+}
+void grab(){
+    moveIntake(true); 
+    moveBelt(true);
+}
+void stop(){
+    Intake.brake();
+    Belt.brake(); 
+}
+
+// void baseAuton(){
+//     chassis.setPose(-57,872,-33.559,0);
+//     chassis.follow(basicAuton_txt,15,2000); 
+//     //chassis.moveToPoint(-60.276,-1.013,4000); //Move to the first point
+// }
+
+void autonOne(){
+    chassis.setPose(-58.44,-23,337,0);
+    chassis.moveToPoint(-59.198,-8.004,0); 
+    grab(); 
+    chassis.moveToPose(-56.169,12.062,50,4000); 
+    stop(); 
+    chassis.moveToPose(-42.161,17.93,120,4000);
+    chassis.moveToPose(-24.745,14.712,180,4000);
+}
+void angularTesting(){
+    // set position to x:0, y:0, heading:0
+    chassis.setPose(0, 0, 0);
+    // turn to face heading 90 with a very long timeout
+    chassis.turnToHeading(90, 100000);
+}
